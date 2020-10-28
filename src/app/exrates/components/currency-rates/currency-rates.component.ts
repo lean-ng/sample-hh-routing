f85c2050572209324bcee21850880753a7ac88f3
Michael Alt <mail@lean-stack.de>
2020-10-28 10:08:39 +0100

    Route parameters.

--- src/app/exrates/components/currency-rates/currency-rates.component.ts
+++ src/app/exrates/components/currency-rates/currency-rates.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-currency-rates',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CurrencyRatesComponent implements OnInit {
 
-  constructor() { }
+  currency = '';
+
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(params =>
+      this.currency = params.get('currency')
+    );
   }
 
+  gotoList(): void {
+    this.router.navigate(['/rates']);
+  }
 }
